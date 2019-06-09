f4acc61689e9c03948f0f219b81f0fe789a8e4b9
Guido Gasper <guido.gasper@googlemail.com>
2019-06-09 16:06:24 +0200

    Nightwatch test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,13 +2,15 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': (browser) => {
+  'Home Dashboard': (browser) => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.containsText('.title', 'Home')
+      .assert.elementPresent(".container")
+      .assert.attributeContains('.container', 'class', '--layout')
+      .assert.cssProperty(".container", "display", "grid")
+      .assert.elementCountGreater(".module", 1)
       .end();
   },
 };
